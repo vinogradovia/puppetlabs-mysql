0f7f44dfcc0a85e616371577cda509ab26b2b83b
Your Name <i.a.vinogradov@telecontact.ru>
2018-02-25 00:32:19 +0300

    new Facts:  * mysql_version_major  * mysql_version_minor  * mysql_version_micro

--- lib/facter/mysql_version.rb
+++ lib/facter/mysql_version.rb
@@ -6,3 +6,30 @@ Facter.add("mysql_version") do
     end
   end
 end
+
+Facter.add("mysql_version_major") do
+  setcode do
+    mysql_ver = Facter.value(:mysql_version)
+    if mysql_ver
+      mysql_ver.split('.')[0]
+    end
+  end
+end
+
+Facter.add("mysql_version_minor") do
+  setcode do
+    mysql_ver = Facter.value(:mysql_version)
+    if mysql_ver
+      mysql_ver.split('.')[1]
+    end
+  end
+end
+
+Facter.add("mysql_version_micro") do
+  setcode do
+    mysql_ver = Facter.value(:mysql_version)
+    if mysql_ver
+      mysql_ver.split('.')[2]
+    end
+  end
+end
